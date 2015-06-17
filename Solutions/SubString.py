1770928cca2605b64dc19d817be77a559c1820ad
Don Johnson <bigdaddybit@gmail.com>
2015-06-17 07:13:47 -0700

    Refactored Substring Solution to be a re-usable function.

--- Solutions/SubString.py
+++ Solutions/SubString.py
@@ -2,24 +2,49 @@
 #author: Don Johnson
 #email: <dj@codetestcode.io>
 # -*- coding: utf-8 -*-
+import string
+
+def subStringSearch(key,source):
+    charList = []
+    indexPosList = []
+    for each_element in key:
+        charList.append(each_element)
+
+    for eachItem in charList:
+        if eachItem in source:
+            indexPosList.append(int(source.index(eachItem)))
+
+    return indexPosList
 
 
- #Step 1.
- #Find the total length of the given string " We love working for Guidance Software, Inc"
-
- #Step 2.
- #With the provided values (7,31,18,24,3,9,5,11,12) create a new string with an expected
- #result of "Good Work"
-
-
-GivenString = "We love working for Guidance Software, Inc"
-StringLength = len(GivenString)
-
-G, o, d = GivenString[31-11],GivenString[9], GivenString[11+12]
-W, r, k = GivenString[21-21],GivenString[5+5], GivenString[11]
+def returnPhrasePos(phrase, source):
+     getIndexPos = subStringSearch(phrase,source)
+     results = ""
+     for indecies in getIndexPos:
+          results += source[indecies]
+     return results
 
 
 
-result = "{}{}{}{} {}{}{}{}".format(G,o,o,d,W,o,r,k)
 
-print(result)
+def main():
+
+    testCharLower = "abcdefghijklmnopqrstuvwxyz "
+    testCharUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+    testCharDigits = "0123456789 "
+    multiLineString = """
+                       EEEEEEEEEEEEEEEEE
+                        c EEEEEEEEEEEEEE
+                        EEEEEEE a EEEEEE
+                        EEEEE t EEEEEEEE
+                      """
+
+
+
+    print(returnPhrasePos("Good Work","I Love Working at Guidance Software."))
+    print(returnPhrasePos("keep it simple stupid",testCharLower))
+    print(returnPhrasePos("cat",multiLineString))
+
+
+if __name__ == "__main__":
+    main()
